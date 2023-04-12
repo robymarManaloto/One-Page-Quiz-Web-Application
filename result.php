8fa255560af078436be965a691ae29b6517f88be
Robymar Louise C. Manaloto <manaloto.robymarlouise@auf.edu.ph>
2023-04-13 01:08:38 +0800

    modified title and added icon.

--- result.php
+++ result.php
@@ -34,9 +34,10 @@ try {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/x-icon" href="icon.png">
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <title>Exam Finished!</title>
+  <title>Result</title>
 </head>
 
 <body>
